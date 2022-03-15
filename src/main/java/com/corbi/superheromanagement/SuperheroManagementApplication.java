72b175675db5aa95a695b0471f4ebc0189e1d53f
vcorbi <vcorbi@atsistemas.com>
2022-03-15 18:14:19 +0100

    Initial commit

---
+++ src/main/java/com/corbi/superheromanagement/SuperheroManagementApplication.java
@@ -0,0 +1,13 @@
+package com.corbi.superheromanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperheroManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperheroManagementApplication.class, args);
+	}
+
+}
